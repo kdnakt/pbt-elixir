8279f7525359bfd0c3d3254c121cddb69363e9fc
kdnakt <a.kid.1985@gmail.com>
2024-02-19 23:26:06 +0900

    implement word counting

--- pbt/test/exercise_test.exs
+++ pbt/test/exercise_test.exs
@@ -28,4 +28,32 @@ defmodule ExerciseTest do
   end
 
   def extract_keys(list), do: for({k, _} <- list, do: k)
+
+  def word_count(chars) do
+    stripped = :string.trim(dedupe_spaces(chars), :both, ' ')
+    spaces = Enum.sum(for char <- stripped, char == ?\s, do: 1)
+
+    case stripped do
+      '' -> 0
+      _ -> spaces + 1
+    end
+  end
+
+  defp dedupe_spaces([]), do: []
+  defp dedupe_spaces([?\s, ?\s | rest]), do: dedupe_spaces([?\s | rest])
+  defp dedupe_spaces([h | t]), do: [h | dedupe_spaces(t)]
+
+  property "word count" do
+    forall chars <- non_empty(char_list()) do
+      word_count(chars) == alt_word_count(chars)
+    end
+  end
+
+  defp alt_word_count(string), do: space(to_charlist(string))
+  defp space([]), do: 0
+  defp space([?\s | str]), do: space(str)
+  defp space(str), do: word(str)
+  defp word([]), do: 1
+  defp word([?\s | str]), do: 1 + space(str)
+  defp word([_ | str]), do: word(str)
 end
