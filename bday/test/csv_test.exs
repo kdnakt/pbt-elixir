99b2cc9a2e2c20f022cbb62301148eeea9cb0a7f
kdnakt <a.kid.1985@gmail.com>
2024-05-12 21:09:45 +0900

    header() and record()

--- bday/test/csv_test.exs
+++ bday/test/csv_test.exs
@@ -1,5 +1,17 @@
 
 
+def header(size) do
+  vector(size, name())
+end
+
+def record(size) do
+  vector(size, field())
+end
+
+def name() do
+  field()
+end
+
 def field() do
   oneof([unquoted_text(), quotable_text()])
 end
