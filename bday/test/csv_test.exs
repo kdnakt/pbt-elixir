ee065bf98293cb4fe3bacd6bfebc92529dae0d2f
kdnakt <a.kid.1985@gmail.com>
2024-05-12 21:28:18 +0900

    use ExUnit

--- bday/test/csv_test.exs
+++ bday/test/csv_test.exs
@@ -1,47 +1,57 @@
+defmodule CsvTest do
+  use ExUnit.Case
+  use PropCheck
 
-def csv_source() do
-  let size <- pos_integer() do
-    let keys <- header(size) do
-      list(entry(size, keys))
+  property "encode and decode csv" do
+    forall maps <- csv_source() do
+      maps == Csv.decode(Csv.encode(maps))
     end
   end
-end
 
-def entry(size, keys) do
-  let vals <- record(size) do
-    Map.new(Enum.zip(keys, vals))
+  def csv_source() do
+    let size <- pos_integer() do
+      let keys <- header(size) do
+        list(entry(size, keys))
+      end
+    end
   end
-end
 
-def header(size) do
-  vector(size, name())
-end
-
-def record(size) do
-  vector(size, field())
-end
-
-def name() do
-  field()
-end
-
-def field() do
-  oneof([unquoted_text(), quotable_text()])
-end
-
-def unquoated_text() do
-  let chars <- list(elements(textdata())) do
-    to_string(chars)
+  def entry(size, keys) do
+    let vals <- record(size) do
+      Map.new(Enum.zip(keys, vals))
+    end
   end
-end
 
-def quotable_text() do
-  let chars <- list(elements('\r\n",' ++ textdata())) do
-    to_string(chars)
+  def header(size) do
+    vector(size, name())
   end
-end
 
-def textdata() do
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' ++
-   ':;<=>?@ !#$%&\'()*+-./[\\]fi_`{|}~'
+  def record(size) do
+    vector(size, field())
+  end
 
+  def name() do
+    field()
+  end
+
+  def field() do
+    oneof([unquoted_text(), quotable_text()])
+  end
+
+  def unquoated_text() do
+    let chars <- list(elements(textdata())) do
+      to_string(chars)
+    end
+  end
+
+  def quotable_text() do
+    let chars <- list(elements('\r\n",' ++ textdata())) do
+      to_string(chars)
+    end
+  end
+
+  def textdata() do
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' ++
+     ':;<=>?@ !#$%&\'()*+-./[\\]fi_`{|}~'
+  end
+end  
