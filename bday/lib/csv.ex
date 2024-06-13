859691082bf050dfdfb88e3f84627f588abdc9f2
kdnakt <a.kid.1985@gmail.com>
2024-06-14 01:17:29 +0900

    fix indent

--- bday/lib/csv.ex
+++ bday/lib/csv.ex
@@ -1,58 +1,51 @@
 defmodule Bday.Csv do
   def encode([]), do: ""
 
-def encode(maps) do
-  keys = Enum.map_join(Map.keys(hd(maps)), ",", &escape(&1))
+  def encode(maps) do
+    keys = Enum.map_join(Map.keys(hd(maps)), ",", &escape(&1))
 
-  vals = 
-    for map <- maps, do: Enum.map_join(Map.values(map), ",", &escape(&1))
-  to_string([keys, "\r\n", Enum.join(vals, "\r\n")])
-end
-
-def decode(""), do: []
-
-def decode(csv) do
-  {headers, rest} = decode_header(csv, [])
-  rows = decode_rows(rest)
-  for row <- rows, do: Map.new(Enum.zip(headers, row))
-end
-
-defp escape(field) do
-  if escapable(field) do
-    ~s|"| <> do_escape(field) <> ~s|"|
-  else
-    field
+    vals = 
+      for map <- maps, do: Enum.map_join(Map.values(map), ",", &escape(&1))
+    to_string([keys, "\r\n", Enum.join(vals, "\r\n")])
   end
-end
 
-defp escapable(string) do
-  String.contains?(string, [~s|"|, ",", "\r", "\n"])
-end
-
-defp do_escape(""), do: ""
-defp do_escape(~s|"| <> str), do: ~s|"| <> do_escape(str)
-defp do_escape(<<char>> <> rest), do: <<char>> <> do_escape(rest)
-
-defp decode_header(string, acc) do
-  case decode_name(string) do
-    {:ok, name, rest} -> decode_header(rest, [name | acc])
-    {:done, name, rest} -> {[name | acc], rest}
+  def decode(""), do: []
+  def decode(csv) do
+    {headers, rest} = decode_header(csv, [])
+    rows = decode_rows(rest)
+    for row <- rows, do: Map.new(Enum.zip(headers, row))
   end
-end
-
-defp decode_rows(string) do
-  case decode_row(string, []) do
-    {row, ""} -> [row]
-    {row, rest} -> [row | decode_rows(rest)]
+  defp escape(field) do
+    if escapable(field) do
+      ~s|"| <> do_escape(field) <> ~s|"|
+    else
+      field
+    end
   end
-end
-
-defp decode_row(string, acc) do
-  case decode_field(string) do
-    {:ok, field, rest} -> decode_row(rest, [field | acc])
-    {:done, field, rest} -> {[field | acc], rest}
+  defp escapable(string) do
+    String.contains?(string, [~s|"|, ",", "\r", "\n"])
   end
-end
+  defp do_escape(""), do: ""
+  defp do_escape(~s|"| <> str), do: ~s|"| <> do_escape(str)
+  defp do_escape(<<char>> <> rest), do: <<char>> <> do_escape(rest)
+  defp decode_header(string, acc) do
+    case decode_name(string) do
+      {:ok, name, rest} -> decode_header(rest, [name | acc])
+      {:done, name, rest} -> {[name | acc], rest}
+    end
+  end
+  defp decode_rows(string) do
+    case decode_row(string, []) do
+      {row, ""} -> [row]
+      {row, rest} -> [row | decode_rows(rest)]
+    end
+  end
+  defp decode_row(string, acc) do
+    case decode_field(string) do
+      {:ok, field, rest} -> decode_row(rest, [field | acc])
+      {:done, field, rest} -> {[field | acc], rest}
+    end
+  end
 
   defp decode_name(~s|"| <> rest), do: decode_quoted(rest)
   defp decode_name(string), do: decode_unquoted(string)
