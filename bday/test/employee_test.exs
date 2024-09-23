ebb91babc191701693170edbad0c22356b249575
kdnakt <a.kid.1985@gmail.com>
2024-09-23 11:51:49 -0700

    raw_employee_map()

--- bday/test/employee_test.exs
+++ bday/test/employee_test.exs
@@ -12,4 +12,30 @@ defmodule EmployeeTest do
     end
   end
 
+  defp raw_employee_map() do
+    let proplist <- [
+          {"last_name", CsvTest.field()},
+          {" first_name", whitespaced_text()},
+          {" date_of_birth", text_date()},
+          {" email", whitespaced_text()}
+        ] do
+      Map.new(proplist)
+    end
+  end
+
+  defp whitespaced_text() do
+    let(txt <- CsvTest.field(), do: " " <> txt)
+  end
+
+  defp text_date() do
+    rawdate = {choose(1900, 2020), choose(1, 12), choose(1, 31)}
+    date = such_that(
+      {y, m, d} <- rawdate,
+      when: {:error, :invalid_date} != Date.new(y, m, d)
+    )
+    let {y, m, d} <- date do
+      IO.chardata_to_string(:io_lib.format(" ~w/~2..0w/~2..0w", [y, m, d]))
+    end
+  end
+
 end
