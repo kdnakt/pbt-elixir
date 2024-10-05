2e3228e55568bebb5193697c1b07a7925b9dadad
kdnakt <a.kid.1985@gmail.com>
2024-10-05 20:27:49 +0900

    parse_date

--- bday/lib/employee.ex
+++ bday/lib/employee.ex
@@ -17,13 +17,23 @@ defmodule Bday.Employee do
   end
   
   defp adapt_csv_result(map) do
-    for {k, v} <- map, into: %{} do
-      {trim(k), maybe_null(trim(v))}
-    end
+    map =
+      for {k, v} <- map, into: %{} do
+        {trim(k), maybe_null(trim(v))}
+      end
+
+    dob = Map.fetch!(map, "date_of_birth")
+    %{map | "date_of_birth" => parse_date(dob)}
   end
   
   defp trim(str), do: String.trim_leading(str, " ")
 
   defp maybe_null(""), do: nil
   defp maybe_null(str), do: str
+
+  defp parse_date(str) do
+    [y, m, d] = Enum.map(String.split(str, "/"), &String.to_integer(&1))
+    {:ok, date} = Date.new(y, m, d)
+    date
+  end
 end
