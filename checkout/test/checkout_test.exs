450341f2df132ff90893e20fce286091b885f36a
kdnakt <a.kid.1985@gmail.com>
2024-12-31 19:45:40 +0900

    add generators

--- checkout/test/checkout_test.exs
+++ checkout/test/checkout_test.exs
@@ -1,8 +1,37 @@
 defmodule CheckoutTest do
   use ExUnit.Case
-  doctest Checkout
+  use PropCheck
 
-  test "greets the world" do
-    assert Checkout.hello() == :world
+  property "no special 1" do
+    forall {item_list, expected_price, price_list} <- item_price_list() do
+      expected_price == Checkout.total(item_list, price_list, [])
+    end
+  end
+
+  ## generators
+  defp item_price_list() do
+    let price_list <- price_list() do
+      let {item_list, expected_price} <- item_list(price_list) do
+        {item_list, expected_price, price_list}
+      end
+    end
+  end
+
+  defp price_list() do
+    let price_list <- non_empty(list({non_empty(utf8()), integer()})) do
+      sorted = Enum.sort(price_list)
+      Enum.dedup_by(sorted, fn {x, _} -> x end)
+    end
+  end
+
+  defp item_list(price_list) do
+    sized(size, item_list(size, price_list, {[], 0}))
+  end
+
+  defp item_list(0, _, acc), do: acc
+  defp item_list(n, price_list, {item_acc, price_acc}) do
+    let {item, price} <- elements(price_list) do
+      item_list(n - 1, price_list, {[item | item_acc], price + price_acc})
+    end
   end
 end
