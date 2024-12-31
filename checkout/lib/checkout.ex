63b1ab1cc8eda85afd05ad799f7c90c69d007fa8
kdnakt <a.kid.1985@gmail.com>
2024-12-31 19:48:39 +0900

    simple implementation of total()

--- checkout/lib/checkout.ex
+++ checkout/lib/checkout.ex
@@ -3,16 +3,11 @@ defmodule Checkout do
   Documentation for `Checkout`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Checkout.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def total(item_list, price_list, _specials) do
+    Enum.sum(
+      for item <- item_list do
+        elem(List.keyfind(price_list, item, 0), 1)
+      end
+    )
   end
 end
