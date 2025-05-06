3ca83762d13d8e5057321d8ea59d856040d6e430
kdnakt <a.kid.1985@gmail.com>
2025-05-06 22:05:40 +0900

    target property project

---
+++ target/lib/target.ex
@@ -0,0 +1,18 @@
+defmodule Target do
+  @moduledoc """
+  Documentation for `Target`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Target.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ target/test/target_test.exs
@@ -0,0 +1,14 @@
+defmodule TargetTest do
+  use ExUnit.Case
+  use PropCheck
+
+  property "always works" do
+    forall type <- term() do
+      boolean(type)
+    end
+  end
+
+  def boolean(_) do
+    true
+  end
+end
