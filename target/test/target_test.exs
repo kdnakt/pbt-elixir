c660ff357547dc2b25d4fad303083f8b3d592957
kdnakt <a.kid.1985@gmail.com>
2025-05-06 22:27:26 +0900

    simple path generator

--- target/test/target_test.exs
+++ target/test/target_test.exs
@@ -11,4 +11,21 @@ defmodule TargetTest do
   def boolean(_) do
     true
   end
+
+  property "path" do
+    forall_targeted p <- path() do
+      result = Enum.reduce(p, {0, 0}, fn direction, {x, y} ->
+        move(direction, {x, y})
+      end)
+      IO.inspect(result)
+      true
+    end
+  end
+
+  def path(), do: list(oneof([:left, :right, :up, :down]))
+
+  def move(:left, {x, y}), do: {x-1, y}
+  def move(:right, {x, y}), do: {x+1, y}
+  def move(:up, {x, y}), do: {x, y+1}
+  def move(:down, {x, y}), do: {x, y-1}
 end
