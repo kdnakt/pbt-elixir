a564d5cae018672247a69323e40794cc87186aaa
kdnakt <a.kid.1985@gmail.com>
2025-05-12 00:57:19 +0900

    test unbalanced tree

--- target/test/target_test.exs
+++ target/test/target_test.exs
@@ -31,4 +31,52 @@ defmodule TargetTest do
   def move(:right, {x, y}), do: {x+1, y}
   def move(:up, {x, y}), do: {x, y+1}
   def move(:down, {x, y}), do: {x, y-1}
+
+  property "tree regular" do
+    forall t <- tree() do
+      weight = sides(t)
+      IO.inspect(weight)
+      true
+    end
+  end
+
+  property "tree" do
+    forall_targeted t <- tree() do
+      weight = sides(t)
+      {left, right} = weight
+      IO.puts("left: #{left}, right: #{right}, weight:#{inspect(weight)}")
+      maximize(left - right)
+      true
+    end
+  end
+
+  def tree() do
+    let l <- non_empty(list(integer())) do
+      to_tree(l)
+    end
+  end
+
+  def to_tree(list) do
+    Enum.reduce(list, nil, fn x, acc ->
+      insert(x, acc)
+    end)
+  end
+
+  def insert(n, {:node, n, l, r}), do: {:node, n, l, r}
+  def insert(n, {:node, m, l, r}) when n < m, do: {:node, m, insert(n, l), r}
+  def insert(n, {:node, m, l, r}) when n > m, do: {:node, m, l, insert(n, r)}
+  def insert(n, {:leaf, n}), do: {:leaf, n}
+  def insert(n, {:leaf, m}) when n < m, do: {:node, n, nil, {:leaf, m}}
+  def insert(n, {:leaf, m}) when n > m, do: {:node, n, {:leaf, m}, nil}
+  def insert(n, nil), do: {:leaf, n}
+
+  def sides({:node, _, l, r}) do
+    {ll, lr} = sides(l)
+    {rl, rr} = sides(r)
+    {count_inner(l) + ll + lr, count_inner(r) + rl + rr}
+  end
+  def sides(_), do: {0, 0}
+
+  def count_inner({:node, _, _, _}), do: 1
+  def count_inner(_), do: 0
 end
