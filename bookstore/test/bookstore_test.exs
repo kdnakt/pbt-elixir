bed8de905605bcd6aec9d625ca41630ff88e0c19
kdnakt <a.kid.1985@gmail.com>
2026-01-12 23:09:38 +0900

    add generator

--- bookstore/test/bookstore_test.exs
+++ bookstore/test/bookstore_test.exs
@@ -1,8 +1,34 @@
 defmodule BookstoreTest do
   use ExUnit.Case
   doctest Bookstore
+  use PropCheck
+  use PropCheck.StateM
 
   test "greets the world" do
     assert Bookstore.hello() == :world
+  end
+
+  def title() do
+    let s <- utf8() do
+      elements([s, String.to_charlist(s)])
+    end
+  end
+
+  def author() do
+    let s <- utf8() do
+      elements([s, String.to_charlist(s)])
+    end
+  end
+
+  def isbn() do
+    let isbn <- [
+      oneof(["978", "979"]),
+      let(x <- range(0, 9999), do: to_charlist(x)),
+      let(x <- range(0, 9999), do: to_charlist(x)),
+      let(x <- range(0, 999), do: to_charlist(x)),
+      frequency([{10, [range(?0, ?9)]}, {1, ["X"]}])
+    ] do
+      to_string(Enum.join(isbn, "-"))
+    end
   end
 end
